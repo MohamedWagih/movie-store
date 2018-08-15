768e08bc4f505d7f8b62ca2ea4f3e4b4bd93b7f9
Mohamed Wagih <mohamed0wagih@gmail.com>
2018-08-15 16:43:23 +0200

    added functionality {filter by genre, sort by column}

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,60 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import { InputLabel, MenuItem, FormControl, Select, Input }from "@material-ui/core";
+
+const styles = theme => ({
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120,
+    
+  },
+  selectEmpty: {
+    marginTop: theme.spacing.unit * 2,
+  },
+  wcolor:{
+    color:theme.palette.common.white,
+  }
+});
+
+class ControlledOpenSelect extends React.Component {
+  state = {
+    genre: "",
+    genresNames:['Action', 'Comedy', 'Thriller']
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <form autoComplete="off">
+        <FormControl className={classes.formControl}>
+          <InputLabel shrink htmlFor="genre-label-placeholder" className={classes.wcolor}>
+            Genre
+          </InputLabel>
+          <Select
+            value={this.state.genre}
+            onChange={this.props.handleChange}
+            input={<Input name="genre" id="genre-label-placeholder" />}
+            displayEmpty
+            name="genre"
+            className={classes.selectEmpty + ' ' + classes.wcolor}
+          >
+            <MenuItem value="">
+              <em>All</em>
+            </MenuItem>
+            <MenuItem value='Action'>Action</MenuItem>
+            <MenuItem value='Thriller'>Thriller</MenuItem>
+            <MenuItem value='Comedy'>Comedy</MenuItem>
+          </Select>
+        </FormControl>
+      </form>
+    );
+  }
+}
+
+ControlledOpenSelect.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(ControlledOpenSelect);
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,13 +2,28 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
-import { TablePagination } from "@material-ui/core";
+import { TablePagination, TableSortLabel } from "@material-ui/core";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import * as movieService from "../../services/fakeMovieService";
+import Filter from '../Filter/Filter';
+
+
+function getSorting(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => b[orderBy] - a[orderBy]
+    : (a, b) => a[orderBy] - b[orderBy];
+}
+
+const columns= [
+  { id: "title", numeric: false, label: "Title" },
+  { id: "genre", numeric: false, label: "Genre" },
+  { id: "numberInStock", numeric: true, label: "Stock" },
+  { id: "dailyRentalRate", numeric: true, label: "Rate" },
+];
 
 const styles = theme => ({
   root: {
@@ -26,8 +41,8 @@ const styles = theme => ({
 
 const CustomTableCell = withStyles(theme => ({
     head: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
+      backgroundColor: theme.palette.common.white,
+      color: theme.palette.common.black,
     },
     body: {
       fontSize: 24,
@@ -38,69 +53,107 @@ class SimpleTable extends Component {
   state = {
     data: movieService.getMovies(),
     page: 0,
-    rowsPerPage: 5
+    rowsPerPage: 5,
+    filter:false,
+    filterBy:'',
+    order: "desc",
+    orderBy: "title",
   };
 
   handleChangePage = (event, page) => {
     this.setState({ page });
   };
 
+  handleFilterRequest = (event) => {
+    if(event.target.value === '')
+      this.setState({filter:false, filterBy:''})
+    else{
+      this.setState({filter:true, filterBy:event.target.value})
+    }
+  }
+
+  handleSortRequest = (property) => {
+    const orderBy = property;
+    let order = "desc";
+
+    if (this.state.orderBy === property && this.state.order === "desc") {
+      order = "asc";
+    }
+
+    this.setState({ order, orderBy });
+  };
+
   render() {
     const { classes } = this.props;
-    const { data, rowsPerPage, page } = this.state;
+    const { data, rowsPerPage, page, filter, filterBy, order, orderBy } = this.state;
     const emptyRows =
       rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
 
     return (
-      <Paper className={classes.root}>
-        <div className={classes.tableWrapper}>
-          <Table className={classes.table}>
-            <TableHead>
-              <TableRow>
-                <CustomTableCell>Title</CustomTableCell>
-                <CustomTableCell>Genre</CustomTableCell>
-                <CustomTableCell numeric>Stock</CustomTableCell>
-                <CustomTableCell numeric>Rate</CustomTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map(row => {
-                  return (
-                    <TableRow key={row.id}>
-                      <TableCell component="th" scope="row">
-                        {row.title}
-                      </TableCell>
-                      <TableCell>{row.genre.name}</TableCell>
-                      <TableCell numeric>{row.numberInStock}</TableCell>
-                      <TableCell numeric>{row.dailyRentalRate}</TableCell>
-                    </TableRow>
-                  );
-                })}
-              {emptyRows > 0 && (
-                <TableRow style={{ height: 48 * emptyRows }}>
-                  <TableCell colSpan={6} />
+      <React.Fragment>
+        <Filter handleChange={this.handleFilterRequest}/>
+        <Paper className={classes.root}>
+          <div className={classes.tableWrapper}>
+            <Table className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  {
+                    columns.map(col=>{
+                      return<CustomTableCell numeric={col.numeric}>
+                        <TableSortLabel
+                          active={orderBy === col.id}
+                          direction={order}
+                          onClick={()=>this.handleSortRequest(col.id)}
+                        >
+                          {col.label}
+                        </TableSortLabel>
+                      </CustomTableCell>
+                    })
+                  }
+                  
                 </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </div>
-        <TablePagination
-          component="div"
-          count={data.length}
-          rowsPerPage={rowsPerPage}
-          rowsPerPageOptions={[]}
-          page={page}
-          backIconButtonProps={{
-            "aria-label": "Previous Page"
-          }}
-          nextIconButtonProps={{
-            "aria-label": "Next Page"
-          }}
-          onChangePage={this.handleChangePage}
-        />
-      </Paper>
+              </TableHead>
+              <TableBody>
+                {data
+                  .sort(getSorting(order, orderBy))
+                  .filter(m=> filter ? m.genre.name === filterBy : true)
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map(movie => {
+                    return (
+                      <TableRow key={movie.id}>
+                        <TableCell component="th" scope="row">
+                          {movie.title}
+                        </TableCell>
+                        <TableCell>{movie.genre.name}</TableCell>
+                        <TableCell numeric>{movie.numberInStock}</TableCell>
+                        <TableCell numeric>{movie.dailyRentalRate}</TableCell>
+                      </TableRow>
+                    );
+                  })}
+                {emptyRows > 0 && (
+                  <TableRow style={{ height: 48 * emptyRows }}>
+                    <TableCell colSpan={6} />
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </div>
+          <TablePagination
+            component="div"
+            count={data.filter(m=> filter ? m.genre.name === filterBy : true).length}
+            rowsPerPage={rowsPerPage}
+            rowsPerPageOptions={[]}
+            page={page}
+            backIconButtonProps={{
+              "aria-label": "Previous Page"
+            }}
+            nextIconButtonProps={{
+              "aria-label": "Next Page"
+            }}
+            onChangePage={this.handleChangePage}
+          />
+        </Paper>
+      </React.Fragment>
     );
   }
 }
